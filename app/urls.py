6f8dfa0d7bb374e0204089be5eb5d018266a8382
Gian Ortiz <brksgian@gmail.com>
2021-09-08 09:07:09 -0300

    Fix: keep max-length of line as 80 characters

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.StockView.as_view(), name='index'),
-    path('products/add', views.ProductCreateView.as_view(), name='products-add'),
-    path('products/<int:pk>/detail', views.ProdutDetailView.as_view(), name='products-detail'),
-    path('products/<int:pk>/delete', views.ProductDeleteView.as_view(), name='products-delete'),
-    path('products/<int:pk>/update', views.ProductUpdateView.as_view(), name='products-update'),
+    path('products/add', views.ProductCreateView.as_view(),
+        name='products-add'),
+    path('products/<int:pk>/detail',
+        views.ProdutDetailView.as_view(), name='products-detail'),
+    path('products/<int:pk>/delete',
+        views.ProductDeleteView.as_view(), name='products-delete'),
+    path('products/<int:pk>/update',
+        views.ProductUpdateView.as_view(), name='products-update'),
 ]
