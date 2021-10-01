d980c25c59a05cdd72a6b2f28ffe82237b6e97a2
Gian Ortiz <brksgian@gmail.com>
2021-10-01 08:48:26 -0300

    Receive hash key as an environment variable

--- app/usuario/usuario.py
+++ app/usuario/usuario.py
@@ -1,5 +1,9 @@
 from hashlib import blake2b
 
+import os
+
+HASH_KEY = os.environ['HASH_KEY']
+
 class Usuario:
 
     def __init__(self, nome: str, senha: str):
@@ -19,7 +23,8 @@ class Usuario:
             raise ValueError('senha deve ser do tipo str')
 
         self.__nome = nome
-        self.__senha = blake2b(bytes(senha, 'utf-8')).hexdigest()
+        self.__senha = blake2b(
+            bytes(senha, 'utf-8'), key=bytes(HASH_KEY, 'utf-8')).hexdigest()
 
     @property
     def nome(self) -> str:
@@ -38,4 +43,5 @@ class Usuario:
         bool
             Retorna True se senhas são iguais ou False caso contrário
         """
-        return self.__senha == blake2b(bytes(senha, 'utf-8')).hexdigest()
+        return self.__senha == blake2b(
+            bytes(senha, 'utf-8'), key=bytes(HASH_KEY, 'utf-8')).hexdigest()
